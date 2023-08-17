ddfcc56fcecbda99da18d6cd1fd9585faa5690c2
romanwalser <142220074+romanwalser@users.noreply.github.com>
2023-08-17 09:01:58 +0200

    Commit from TeleportHQ

---
+++ src/views/home.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import './home.css'
+
+const Home = (props) => {
+  return (
+    <div className="home-container">
+      <Helmet>
+        <title>Glossy Precious Woodpecker</title>
+        <meta property="og:title" content="Glossy Precious Woodpecker" />
+      </Helmet>
+      <a
+        href="https://test.kamadeta.com/"
+        target="_blank"
+        rel="noreferrer noopener"
+        className="home-link"
+      >
+        <img alt="image" src="/buttonbg-400w.jpg" className="home-image" />
+      </a>
+    </div>
+  )
+}
+
+export default Home
