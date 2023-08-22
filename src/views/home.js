ead53394bbd1f62a7f357d6822aa802a5bc8dda8
romanwalser <142220074+romanwalser@users.noreply.github.com>
2023-08-22 07:38:33 +0200

    Commit from TeleportHQ

--- src/views/home.js
+++ src/views/home.js
@@ -8,11 +8,16 @@ const Home = (props) => {
   return (
     <div className="home-container">
       <Helmet>
-        <title>Glossy Precious Woodpecker</title>
-        <meta property="og:title" content="Glossy Precious Woodpecker" />
+        <title>Recious Flas</title>
+        <meta property="og:title" content="Recious Flas" />
       </Helmet>
-      <a href="https://test.kamadeta.com/" className="home-link">
-        <img alt="image" src="/buttonbg-400w.jpg" className="home-image" />
+      <a
+        href="https://nlpl1.kalkuala.homes/"
+        target="_blank"
+        rel="noreferrer noopener"
+        className="home-link"
+      >
+        <img alt="image" src="/nlbutton-400h.jpg" className="home-image" />
       </a>
     </div>
   )
